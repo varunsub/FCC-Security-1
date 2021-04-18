bbf53bf357f7c7ef8de7ce6ed8df7310f6d69fea
varunsub <varun.subramanyan@gmail.com>
2021-04-17 21:03:35 -0700

    Added helmet

--- myApp.js
+++ myApp.js
@@ -1,58 +1,14 @@
 var express = require('express');
 var app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require('helmet');
 
 module.exports = app;
 var api = require('./server.js');
+app.use(helmet());
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 var listener = app.listen(process.env.PORT || 3000, function () {
